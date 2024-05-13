224c8141537ef46c771ef9d0323564d5c8f74827
Adamocho <81510507+Adamocho@users.noreply.github.com>
2024-05-13 21:35:46 +0200

    Implemented home-manager

--- configuration.nix
+++ configuration.nix
@@ -2,11 +2,14 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
-    [ ./hardware-configuration.nix ];
+    [ # Hardware scan
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+    ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -92,14 +95,23 @@
     ];
   };
 
+ # home-manager = {
+ #   extraSpecialArgs = { inherit inputs; };
+ #   users = {
+ #     "adam" = import ./home.nix;
+ #   };
+ # };
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  vim
-  git
+    vim
+    git
+    neovim
+    neofetch
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
--- flake.nix
+++ flake.nix
@@ -1,12 +1,20 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = { self, nixpkgs, ... } @inputs: {
     nixosConfigurations.earth = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
-      modules = [ ./configuration.nix ];
+      modules = [ 
+        ./configuration.nix
+	inputs.home-manager.nixosModules.default
+      ];
     };
   };
 }
