aaff62235eb075cba0ab6e7143ce02685e3272c8
Adamocho <81510507+Adamocho@users.noreply.github.com>
2024-08-21 19:12:53 +0200

    Create a modular nixos config

--- flake.nix
+++ flake.nix
@@ -1,16 +1,28 @@
 {
- description = "earth (pc) nix flake";
+ description = "machines' nixos config";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
   };
   outputs = { self, nixpkgs, ... } @inputs: {
-    nixosConfigurations.earth = nixpkgs.lib.nixosSystem rec {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [ 
-        ./configuration.nix
-      ];
+    nixosConfigurations = {
+
+      earth = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [ 
+          ./hosts/earth/configuration.nix
+        ];
+      };
+
+      # moon = nixpkgs.lib.nixosSystem {
+      #   system = "x86_64-linux";
+      #   specialArgs = { inherit inputs; };
+      #   modules = [ 
+      #     ./hosts/moon/configuration.nix
+      #   ];
+      # };
     };
+
   };
 }
