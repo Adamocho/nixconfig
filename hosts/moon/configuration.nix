52e940e77a6e62f6820637d6e8f57fdf70d42579
Adamocho <81510507+Adamocho@users.noreply.github.com>
2024-09-17 18:47:53 +0200

    Add custom power settings using tlp

--- hosts/moon/configuration.nix
+++ hosts/moon/configuration.nix
@@ -210,11 +210,35 @@
     enable = true;
     enableSSHSupport = true;
   };
-
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+
+  # Turn something else off 
+  services.power-profiles-daemon.enable = false;
+
+  # Power settings
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 50;
+
+     #Optional helps save long term battery health
+     START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+     STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+
+    };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
